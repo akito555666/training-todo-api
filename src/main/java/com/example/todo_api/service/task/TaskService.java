b2e172596d0f3360f6d9509057f5282180a3d308
Akito Yamauchi <akito555666@gmail.com>
2025-11-10 12:23:34 +0900

    Update build command in README.md

--- src/main/java/com/example/todo_api/service/task/TaskService.java
+++ src/main/java/com/example/todo_api/service/task/TaskService.java
@@ -1,8 +1,14 @@
 package com.example.todo_api.service.task;
 
+import com.example.todo_api.repository.task.TaskRecord;
 import com.example.todo_api.repository.task.TaskRepository;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +20,19 @@ public class TaskService {
         return taskRepository.select(taskId)
                 .map(record -> new TaskEntity(record.getId(), record.getTitle()))
                 .orElseThrow(() -> new TaskEntityNotFoundException(taskId));
+    }
+
+    public List<TaskEntity> find(int limit, long offset) {
+        return taskRepository.selectList(limit, offset)
+                .stream()
+                .map(record -> new TaskEntity(record.getId(), record.getTitle()))
+                .toList();
+    }
+
+    public TaskEntity create(@NotNull String title) {
+        var record = new TaskRecord(null, title);
+        taskRepository.insert(record);
+
+        return new TaskEntity(record.getId(), record.getTitle());
     }
 }
